3b579d11978cb7d8034f28d46c5fe3ec7e31702b
Simon Olander <simon.olander@sap.com>
2025-06-23 16:30:16 +0200

    docs(README): updates readme (and adds some missing comments)
    ---------
    
    on-behalf-of: @SAP Simon Olander (simon.olander@sap.com)

--- watcher.go
+++ watcher.go
@@ -27,6 +27,13 @@ type watcher interface {
 	Watch(path string, event Event, is_recursive bool) error
 
 	// Unwatch requests a watcher deletion for the given path and given event set.
+	//
+	// When recursive:
+	//	Removes a recursive watch-point given by the path. For
+	//	native recursive implementation there is no difference in functionality
+	//	between Unwatch and RecursiveUnwatch, however for those platforms, that
+	//	requires emulation for recursive watch-points, the implementation differs.
+
 	Unwatch(path string, is_recursive bool) error
 
 	// Rewatch provides a functionality for modifying existing watch-points, like
@@ -40,6 +47,34 @@ type watcher interface {
 	// of its arguments. If old == new and watcher can be upgraded to
 	// recursiveWatcher interface, a watch for the corresponding path is expected
 	// to be changed from recursive to the non-recursive one.
+
+	// When recursive:
+	//	Provides a functionality for modifying and/or relocating
+	//	existing recursive watch-points.
+	//
+	//	To relocate a watch-point means to unwatch oldpath and set a watch-point on
+	//	newpath.
+	//
+	//	To modify a watch-point means either to expand or shrink its event set.
+	//
+	//	tree can want to either relocate, modify or relocate and modify a watch-point
+	//	via single RecursiveRewatch call.
+	//
+	//	If oldpath == newpath, the watch-point is expected to change its event set value
+	//	from oldevent to newevent.
+	//
+	//	If oldevent == newevent, the watch-point is expected to relocate from oldpath
+	//	to the newpath.
+	//
+	//	If oldpath != newpath and oldevent != newevent, the watch-point is expected
+	//	to relocate from oldpath to the newpath first and then change its event set
+	//	value from oldevent to the newevent. In other words the end result must be
+	//	a watch-point set on newpath with newevent value of its event set.
+	//
+	//	It is guaranteed that tree will not pass to RecurisveRewatcha zero value
+	//	for any of its arguments. If oldpath == newpath and oldevent == newevent,
+	//	a watch for the corresponding path is expected to be changed for
+	//	non-recursive to the recursive one.
 	Rewatch(oldpath, newpath string, old, new Event, is_recursive bool) error
 
 	// Close unwatches all paths that are registered. When Close returns, it
